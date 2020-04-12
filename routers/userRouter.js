33e6a6a283fbcd4dc963dc589ec33cffc13d7eac
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-04-13 04:22:48 +0900

    #2.10 MVC Pattern part Two

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
+userRouter.get(routes.changePassword, (req, res) =>
+  res.send("Change Password")
+);
+userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
 
 export default userRouter;
